9e2cc5557c57a6008cf4d5061d66e108a2710ba5
Jan Sandbrink <fischikowski@web.de>
2011-04-11 21:36:33 +0200

    init repository with some files

---
+++ CompareFS/ModificationType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CompareFS
+{
+    enum ModificationType
+    {
+        None,
+        Added,
+        Removed,
+        Changed
+    }
+}
---
+++ CompareFS/PathModification.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CompareFS
+{
+    abstract class PathModification
+    {
+        public abstract string Path { get; }
+    }
+}
