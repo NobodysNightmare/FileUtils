8d6d8cfaf5a132cc89a615259068023171d0d858
Jan Sandbrink <fischikowski@web.de>
2013-04-04 12:35:09 +0200

    add FileBloater

---
+++ FileBloater/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace FileBloater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int bloatSize = 8192;
+            var bloatBuffer = new byte[bloatSize];
+            new Random().NextBytes(bloatBuffer);
+            long loopCount = 0;
+            using (var file = File.OpenWrite("foo.bloat"))
+            {
+                try
+                {
+                    while (true)
+                    {
+                        file.Write(bloatBuffer, 0, bloatBuffer.Length);
+                        if(++loopCount % 1024 == 0)
+                            Console.WriteLine("{0} MiB written so far.", (loopCount*bloatSize)/(1024*1024)); ;
+                    }
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine("Error occured: {0}", e.Message);
+                }
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -18,3 +18,10 @@ This tool will write a single byte to the drive in a regular interval, this prev
 
 To keep things green the tool will stop writing when there is no user-interaction with the machine for a certain amount of time.
 This means the drive will spin down when I am not at my computer.
+
+FileBloater
+-----------
+
+This convenient little tool will create a file in its own directory and increase its size until an error occures.
+This can be helpful if your hard drive encounters bad sectors that it needs to be written to in order to try to re-allocate them.
+Doing it this way is quite safe, as it will only consume unallocated space, so it is not possible that you will erase existing file contents (except for hard-drive-failure due to write-activity). However, you will also only be able to fix sectors that don't belong to allocated files.
